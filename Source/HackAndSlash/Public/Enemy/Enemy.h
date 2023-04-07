2ece56e35b2581415c30a26cc09f3a30d89f0c02
Pierson Trimarchi <pstrimar@gmail.com>
2023-04-07 15:36:43 -0400

    Implementing hit interface

--- Source/HackAndSlash/Public/Enemy/Enemy.h
+++ Source/HackAndSlash/Public/Enemy/Enemy.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Interfaces/HitInterface.h"
 #include "Enemy.generated.h"
 
 UCLASS()
-class HACKANDSLASH_API AEnemy : public ACharacter
+class HACKANDSLASH_API AEnemy : public ACharacter, public IHitInterface
 {
 	GENERATED_BODY()
 
@@ -15,6 +16,7 @@ public:
 	AEnemy();
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void GetHit(const FVector& ImpactPoint) override;
 
 protected:
 	virtual void BeginPlay() override;
